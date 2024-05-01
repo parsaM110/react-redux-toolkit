c1e48fac5195182752ccfca95af862cd61eeb893
parsaM110 <pmomeni80@gmail.com>
2024-05-01 17:21:05 +0330

    using incrementByAmount in div (red) and custom input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, logCount, reset, incrementByAmount } from '../features/counter/counterSlice';
 
 function Counter() {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch(); // get the dispatch function
+    const [incrementAmount, setIncrementAmount] = useState(1);
 
     return (
         <div>
@@ -14,6 +15,10 @@ function Counter() {
             <button onClick={() => dispatch(logCount())}>log</button>
             <button onClick={() => dispatch(reset())}>reset</button>
             <button onClick={() => dispatch(incrementByAmount(5))}>increment by 5</button>
+            <div style={{backgroundColor: "red"}}>
+                <input value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+                <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>increment by amount {incrementAmount}</button>
+            </div>
         </div>
     );
 }
