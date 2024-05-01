72990d155d7bfbe941a76669aed2654230f418fe
parsaM110 <pmomeni80@gmail.com>
2024-05-01 16:50:47 +0330

    adding buttons to control Counter | you register it on store but import it from couterSlice

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../features/counter/counterSlice';
+import { increment, decrement, logCount } from '../features/counter/counterSlice';
 
 function Counter() {
     const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch(); // get the dispatch function
+
     return (
         <div>
             {count.toString()}
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(logCount())}>log</button>
         </div>
     );
 }
